65b88e94ed582159a537d1ff2bb7eff8efc8f102
Nizo <nizo@example.com>
2026-07-16 16:04:48 +0200

    Add User:So9q to User-Agent header for Wikimedia API calls

--- matcher/__init__.py
+++ matcher/__init__.py
@@ -17,7 +17,7 @@ class Entity(typing.TypedDict):
 
 
 user_agent = (
-    "osm-wikidata/0.1 (https://github.com/EdwardBetts/osm-wikidata; edward@4angle.com)"
+    "osm-wikidata/0.1 (https://github.com/EdwardBetts/osm-wikidata; edward@4angle.com; User:So9q)"
 )
 
 default_change_comments = {
